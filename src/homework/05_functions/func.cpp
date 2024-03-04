f10ef454bfcc90894287976c4c16e2446f9fd880
CesarMontano1 <157077890+CesarMontano1@users.noreply.github.com>
2024-03-04 01:31:29 +0000

    Homework 5 Solution

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,50 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+double get_gc_content(const std::string& dna) {
+    int gc_count = 0;
+
+    for (char nucleotide : dna) {
+        if (nucleotide == 'G' || nucleotide == 'C') {
+            gc_count++;
+        }
+    }
+
+    return static_cast<double>(gc_count) / dna.length();
+}
+
+std::string reverse_string(const std::string& dna) {
+    std::string reversed = "";
+
+    for (int i = dna.length() - 1; i >= 0; i--) {
+        reversed += dna[i];
+    }
+
+    return reversed;
+}
+
+std::string get_dna_complement(const std::string& dna) {
+    std::string complement = reverse_string(dna);
+
+    for (char& nucleotide : complement) {
+        switch (nucleotide) {
+            case 'A':
+                nucleotide = 'T';
+                break;
+            case 'T':
+                nucleotide = 'A';
+                break;
+            case 'C':
+                nucleotide = 'G';
+                break;
+            case 'G':
+                nucleotide = 'C';
+                break;
+            default:
+                // Handle invalid characters if necessary
+                break;
+        }
+    }
+
+    return complement;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Function prototypes
+double get_gc_content(const string& dna);
+string get_dna_complement(const string& dna);
 
 int main() 
 {
+	int choice;
+    string dna;
+
+    do {
+        // Display menu
+        cout << "\nMenu:\n";
+        cout << "1- Get GC Content\n";
+        cout << "2- Get DNA Complement\n";
+        cout << "3- Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter DNA string: ";
+                cin >> dna;
+                cout << "GC Content: " << get_gc_content(dna) << endl;
+                break;
+            case 2:
+                cout << "Enter DNA string: ";
+                cin >> dna;
+                cout << "DNA Complement: " << get_dna_complement(dna) << endl;
+                break;
+            case 3:
+                cout << "Exiting program.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please enter a valid option.\n";
+        }
+
+    } while (choice != 3);
 
 	return 0;
 }
